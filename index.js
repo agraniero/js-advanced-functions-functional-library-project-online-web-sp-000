fd78276fc40512e52e7fb4682ec14b5df5c373b5
Anthony Graniero <ananfro@gmail.com>
2019-12-12 17:28:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fi = (function() {
     each: function(collection, callback) {
       for (var i=0; i<collection.length; i++)
       callback(collection[i], i, collection);
-      collection
+      return collection
     },
 
     map: function() {
